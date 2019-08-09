583abbd74f31eb59388b2c952aa4a2e2ef098c37
chenfanfang <493336001@qq.com>
2019-08-09 10:24:37 +0800

    edit

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import VueAwesomeSwiper from 'vue-awesome-swiper';
 import './assets/styles/reset.css' 
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+
+//vue-awesome-swiper
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper, /* { default global options } */)
 
 Vue.use(VueAwesomeSwiper)
 
